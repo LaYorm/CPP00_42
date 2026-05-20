4f8be49dda2f1583f9646b5f37664e9a258978e8
yorimek <yorimek@f4r2s11.paris.42.school>
2026-05-20 11:57:34 +0200

    cpp00/ex00 finish

---
+++ ex00/megaphone.cpp
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   megaphone.cpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yorimek <yorimek@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/05/19 16:58:44 by yorimek           #+#    #+#             */
+/*   Updated: 2026/05/19 17:02:48 by yorimek          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+#include <ctype.h>
+
+int	main(int argc, char **argv)
+{
+	int	i;
+	int	j;
+
+	if (argc > 1)
+	{
+		i = 1;
+		while (argv[i])
+		{
+			j = 0;
+			while (argv[i][j])
+			{
+				std::cout << (char)toupper(argv[i][j]);
+				j++;
+			}
+			i++;
+		}
+		std::cout << std::endl;
+	}
+	else
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+}
