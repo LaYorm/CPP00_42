4d0c9741097a1f7460ed836eec921aa007140f51
yorimek <yorimek@f4r2s9.paris.42.school>
2026-06-01 12:26:23 +0200

    update: ex02 Dream job finished

---
+++ ex02/Account.cpp
@@ -0,0 +1,153 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Account.cpp                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yorimek <yorimek@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/05/29 16:42:21 by yorimek           #+#    #+#             */
+/*   Updated: 2026/06/01 12:20:12 by yorimek          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Account.hpp"
+#include "iostream"
+#include "ctime"
+
+int	Account::_nbAccounts = 0;
+
+int	Account::_totalAmount = 0;
+
+int	Account::_totalNbDeposits = 0;
+
+int	Account::_totalNbWithdrawals = 0;
+
+Account::Account (int initial_deposit)
+{
+	this->_amount = initial_deposit;
+	this->_nbDeposits = 0;
+	this->_nbWithdrawals = 0;
+	this->_accountIndex = getNbAccounts();
+	Account::_totalAmount += this->_amount;
+	Account::_displayTimestamp();
+	std::cout << " index:" << this->_accountIndex << ";amount:" << this->_amount
+		<< ";created" << std::endl;
+	Account::_nbAccounts++;
+}
+
+Account::~Account(void)
+{
+	Account::_displayTimestamp();
+	std::cout << " index:" << this->_accountIndex << ";amount:" << this->_amount
+		<< ";closed" << std::endl;
+}
+
+void	Account::displayAccountsInfos(void)
+{
+	Account::_displayTimestamp();
+	std::cout << " accounts:" << getNbAccounts() << ";total:"
+		<< getTotalAmount() << ";deposits:" << getNbDeposits()
+		<< ";withdrawals:" << getNbWithdrawals() << std::endl;
+}
+
+void	Account::_displayTimestamp(void)
+{
+	char	time_now[18];
+	time_t	now;
+	tm		*ltm;
+	
+	now = time(NULL);
+	ltm = localtime(&now);
+	std::strftime(time_now, 18, "[%Y%m%d_%H%M%S]", ltm);
+	std::cout << time_now;
+}
+
+
+void	Account::makeDeposit(int deposit)
+{
+	this->_nbDeposits++;
+	Account::_displayTimestamp();
+	std::cout << " index:" << this->_accountIndex << ";p_amount:"
+		<< this->_amount << ";deposit:" << deposit;
+	this->_amount += deposit;
+	std::cout << ";amount:" << this->_amount << ";nb_deposits:" << this->_nbDeposits
+		<< std::endl;
+	Account::_totalNbDeposits++;
+	Account::_totalAmount += deposit;
+}
+
+bool	Account::makeWithdrawal(int withdrawal)
+{
+	Account::_displayTimestamp();
+	std::cout << " index:" << this->_accountIndex << ";p_amount:"
+		<< this->_amount << ";withdrawal:";
+	if (withdrawal > this->_amount)
+	{
+		std::cout << "refused" << std::endl;
+		return (false);
+	}
+	else
+	{
+		this->_amount -= withdrawal;
+		std::cout << withdrawal << ";amount:" << this->_amount;
+		this->_nbWithdrawals++;
+		std::cout << ";nb_withdrawals:" << this->_nbWithdrawals << std::endl;
+		Account::_totalNbWithdrawals++;
+		Account::_totalAmount -= withdrawal;
+		return (true);
+	}
+}
+
+int	Account::checkAmount(void) const
+{
+	return (this->_amount);
+}
+
+void	Account::displayStatus(void) const
+{
+	Account::_displayTimestamp();
+	std::cout << " index:" << this->_accountIndex << ";amount:";
+	std::cout << this->_amount << ";deposits:" << this->_nbDeposits;
+	std::cout << ";withdrawals:" << this->_nbWithdrawals << std::endl;
+}
+
+int	Account::getNbAccounts(void)
+{
+	return (_nbAccounts);
+}
+
+int	Account::getTotalAmount(void)
+{
+	return (_totalAmount);
+}
+
+int	Account::getNbDeposits(void)
+{
+	return (_totalNbDeposits);
+}
+
+int	Account::getNbWithdrawals(void)
+{
+	return (_totalNbWithdrawals);
+}
+
+// int main(void)
+// {
+// 	int	i;
+// 	int	amounts[] = { 42, 54};
+
+// 	i = 0;
+// 	while (i < 2)
+// 	{
+// 		Account test(amounts[i]);
+// 		Account::displayAccountsInfos();
+// 		test.displayStatus();
+// 		test.makeWithdrawal(50);
+// 		test.makeDeposit(50);
+// 		test.makeDeposit(50);
+// 		test.makeWithdrawal(10);
+// 		Account::displayAccountsInfos();
+// 		test.displayStatus();
+// 		i++;
+// 	}
+// }
